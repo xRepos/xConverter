349b4cb08d2f11a49deb331ed9a210d014f9d9e7
x <xxdemo@126.com>
2013-12-26 14:18:31 -0800

    add csv2table

--- converter.js
+++ converter.js
@@ -69,6 +69,64 @@ var csv2geojson = function(csvRawString, delimiter, hasHeader) {
 
 }
 
+var csv2htmlTable = function(raw, options) {
+
+    var defaults = {
+        delimiter: ",",
+        hasHeader: false
+    }
+
+    options = getMergedOptions(options, defaults);
+
+ 
+
+    var tableHtmlList = ["<table>"];
+
+    if (options.hasHeader == false) {
+        tableHtmlList.push("<tbody>")
+    }
+
+    raw.trim().split("\n").forEach(function(row) {
+        if (options.hasHeader) {
+            tableHtmlList.push("<thead>")
+        }
+
+        rowHtmlList = ["<tr>"];
+        row.trim().split(options.delimiter).forEach(function(cell) {
+            if (options.hasHeader) {
+                rowHtmlList.push("<th>" + cell + "</th>");
+            } else {
+                rowHtmlList.push("<td>" + cell + "</td>");
+            }
+        });
+        rowHtmlList.push("</tr>")
+
+        tableHtmlList.push(rowHtmlList.join(""))
+
+        if (options.hasHeader) {
+            tableHtmlList.push("</thead><tbody>");
+            options.hasHeader = false;
+        }
+    })
+
+    tableHtmlList.push("</tbody></table>")
+    return tableHtmlList.join("");
+}
+
+var getMergedOptions = function(options, defaults) {
+    var options = options || {}, 
+        option;
+
+    for(option in defaults) {
+        if(defaults.hasOwnProperty(option) && !options[option]) {
+            options[option] = defaults[option];
+        }
+    }
+
+    return options;
+}
+
 exports.csv2json = csv2json;
 exports.objects2geojson = objects2geojson;
 exports.csv2geojson = csv2geojson;
+exports.csv2htmlTable = csv2htmlTable;
